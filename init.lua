47b3c1bea95b0ff0e8d9d2d264319db11cef6cac
ysl2 <ysl2@88.com>
2022-12-29 07:47:07 +0800

    1

--- init.lua
+++ init.lua
@@ -494,7 +494,7 @@ require('lualine').setup({
   options = {
     theme = 'tokyonight',
     section_separators = { left = '', right = '' },
-    component_separators = { left = '|', right = '|' }
+    component_separators = { left = '', right = '' }
   }
 })
 
