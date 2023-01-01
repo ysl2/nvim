502c9b45fe30c061ac411adcd757282277965907
ysl2 <ysl2@88.com>
2023-01-01 23:44:15 +0800

    1

--- init.lua
+++ init.lua
@@ -528,8 +528,6 @@ require("nvim-tree").setup({
       list = {
         { key = { "l", "<CR>", "o" }, action = "edit", mode = "n" },
         { key = "h", action = "close_node" },
-        { key = "v", action = "vsplit" },
-        { key = "C", action = "cd" },
       }
     }
   }
