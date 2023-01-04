2f70219633440b876fd47c0947e7ca814a0ec199
Songli Yu <39717545+ysl2@users.noreply.github.com>
2023-01-05 00:35:54 +0800

    '1'

--- init.lua
+++ init.lua
@@ -681,31 +681,40 @@ end
 require('cmp').setup({
   completion = { completeopt = 'menu,menuone' },
   snippet = {
-    -- REQUIRED - you must specify a snippet engine
     expand = function(args)
-      -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
       require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-      -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-      -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
     end,
-  },
-  window = {
-    -- completion = cmp.config.window.bordered(),
-    -- documentation = cmp.config.window.bordered(),
   },
   mapping = require('cmp').mapping.preset.insert({
     ['<C-b>'] = require('cmp').mapping.scroll_docs(-4),
     ['<C-f>'] = require('cmp').mapping.scroll_docs(4),
     ['<C-Space>'] = require('cmp').mapping.complete(),
-    ['<C-e>'] = require('cmp').mapping.abort(),
-    ['<CR>'] = require('cmp').mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    ['<CR>'] = require('cmp').mapping.confirm {
+      behavior = require('cmp').ConfirmBehavior.Replace,
+      select = true,
+    },
+    ['<Tab>'] = require('cmp').mapping(function(fallback)
+      if require('cmp').visible() then
+        require('cmp').select_next_item()
+      elseif require('luasnip').expand_or_jumpable() then
+        require('luasnip').expand_or_jump()
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+    ['<S-Tab>'] = require('cmp').mapping(function(fallback)
+      if require('cmp').visible() then
+        require('cmp').select_prev_item()
+      elseif require('luasnip').jumpable(-1) then
+        require('luasnip').jump(-1)
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
   }),
   sources = require('cmp').config.sources({
     { name = 'nvim_lsp' },
-    -- { name = 'vsnip' }, -- For vsnip users.
     { name = 'luasnip' }, -- For luasnip users.
-    -- { name = 'ultisnips' }, -- For ultisnips users.
-    -- { name = 'snippy' }, -- For snippy users.
   }, {
     { name = 'buffer' },
   }),
