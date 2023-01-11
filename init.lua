51548543b2822c9d6e131c7d7465e6300b7770b9
Songli Yu <39717545+ysl2@users.noreply.github.com>
2023-01-11 12:13:40 +0800

    '1'

--- init.lua
+++ init.lua
@@ -6,6 +6,7 @@ vim.g.neovide_cursor_animation_length = 0
 vim.opt.wrap = false
 vim.opt.scrolloff = 1
 vim.opt.maxmempattern = 2000
+vim.opt.signcolumn = 'yes'
 
 vim.opt.number = true
 vim.opt.relativenumber = true
