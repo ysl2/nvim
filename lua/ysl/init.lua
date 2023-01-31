8778e4ecac51a887a58b015ef016dbdf01edf8c1
Songli Yu <39717545+ysl2@users.noreply.github.com>
2023-02-01 00:37:01 +0800

    1

--- lua/ysl/init.lua
+++ lua/ysl/init.lua
@@ -182,6 +182,7 @@ if vim.fn.has('win32') == 0 then
   M[#M + 1] = {
     'kevinhwang91/rnvimr',
     keys = { { '<Leader>r', ':RnvimrToggle<CR>', mode = 'n', silent = true } },
+    cmd = 'Rnvimr',
     config = function()
       vim.g.rnvimr_enable_picker = 1
       vim.g.rnvimr_enable_bw = 1
@@ -200,6 +201,7 @@ end
 if vim.fn.has('win32') == 0 then
   M[#M + 1] = {
     'kdheepak/lazygit.nvim',
+    cmd = 'LazyGit',
     keys = { { '<Leader>g', ':LazyGit<CR>', mode = 'n', silent = true } },
   }
 end
