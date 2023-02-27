6c3d841db98146b77018fa6f68ac7acfc369e4e4
ysl2 <www.songli.yu@gmail.com>
2023-02-27 22:23:58 +0800

    1

--- lua/ysl/init.lua
+++ lua/ysl/init.lua
@@ -537,8 +537,8 @@ vim.list_extend(M, {
     config = function()
       if vim.fn.has('win32') == 1 then
         local powershell_options = {
-          shell = vim.fn.executable 'pwsh' == 1 and 'pwsh' or 'powershell',
-          shellcmdflag = '-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;',
+          shell = (vim.fn.executable 'pwsh' == 1 and 'pwsh' or 'powershell') .. ' -NoLogo -ExecutionPolicy RemoteSigned',
+          shellcmdflag = '-Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;',
           shellredir = '-RedirectStandardOutput %s -NoNewWindow -Wait',
           shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode',
           shellquote = '',
