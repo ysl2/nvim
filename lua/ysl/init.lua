4d8997986ba4889dbf13f95f782858ada0f94bb1
ysl2 <www.songli.yu@gmail.com>
2023-03-10 12:30:51 +0800

    1

--- lua/ysl/init.lua
+++ lua/ysl/init.lua
@@ -913,6 +913,7 @@ vim.list_extend(M, {
       vim.g.mkdp_port = '8080'
       vim.g.mkdp_open_to_the_world = 1
       vim.g.mkdp_echo_preview_url = 1
+      vim.g.mkdp_open_ip = '127.0.0.1'
     end
   },
 })
