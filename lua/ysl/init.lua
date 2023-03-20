d91cdc96283618fad6e708d994f016cd9922d1b9
ysl2 <www.songli.yu@gmail.com>
2023-03-20 08:33:49 +0800

    1

--- lua/ysl/init.lua
+++ lua/ysl/init.lua
@@ -855,7 +855,6 @@ vim.list_extend(M, {
     lazy = false,
     config = function()
       require('transparent').setup({
-        enable = true,   -- boolean: enable transparent
         extra_groups = { -- table/string: additional groups that should be cleared
           -- In particular, when you set it to 'all', that means all available groups
           'lualine_a_inactive',
@@ -929,7 +928,7 @@ vim.list_extend(M, {
           'Pmenu',
           'VertSplit',
         },
-        exclude = {}, -- table: groups you don't want to clear
+        exclude_groups = {}, -- table: groups you don't want to clear
         -- ignore_linked_group = true, -- boolean: don't clear a group that links to another group
       })
     end
