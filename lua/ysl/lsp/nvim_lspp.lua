42029dac218ef2c9ca92d3e0cdc677b27fe17534
ysl2 <www.songli.yu@gmail.com>
2023-07-31 11:37:10 +0800

    Update

--- lua/ysl/lsp/nvim_lspp.lua
+++ lua/ysl/lsp/nvim_lspp.lua
@@ -158,6 +158,20 @@ return {
               { name = 'luasnip' },
             },
           }
+
+          vim.diagnostic.config({
+            virtual_text = true,
+            signs = true,
+            underline = true,
+            update_in_insert = true,
+            severity_sort = false,
+          })
+
+          local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+          for type, icon in pairs(signs) do
+            local hl = "DiagnosticSign" .. type
+            vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+          end
         end
       },
       'b0o/schemastore.nvim'
