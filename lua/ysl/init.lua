6657c6b65989c429817c2b957dfe3aa84bc358e0
ysl2 <www.songli.yu@gmail.com>
2023-08-02 09:39:11 +0800

    Add nvim-spectre

--- lua/ysl/init.lua
+++ lua/ysl/init.lua
@@ -935,16 +935,24 @@ vim.list_extend(M, {
   },
   {
     'kevinhwang91/nvim-hlslens',
+    keys = {
+      { '/' },
+      { '?' },
+      { 'n', [[<Cmd>execute('normal! ' . v:count1 . 'n')<CR><Cmd>lua require('hlslens').start()<CR>]], mode = { 'n', 'v' }, silent = true },
+      { 'N', [[<Cmd>execute('normal! ' . v:count1 . 'N')<CR><Cmd>lua require('hlslens').start()<CR>]], mode = { 'n', 'v' }, silent = true },
+      { '*', [[*<Cmd>lua require('hlslens').start()<CR>]], mode = { 'n', 'v' }, silent = true },
+      { '#', [[#<Cmd>lua require('hlslens').start()<CR>]], mode = { 'n', 'v' }, silent = true },
+      { 'g*', [[g*<Cmd>lua require('hlslens').start()<CR>]], mode = { 'n', 'v' }, silent = true },
+      { 'g#', [[g#<Cmd>lua require('hlslens').start()<CR>]], mode = { 'n', 'v' }, silent = true },
+    },
     config = function()
       require('hlslens').setup()
-      local opts = {noremap = true, silent = true}
-      vim.keymap.set('n', 'n', [[<Cmd>execute('normal! ' . v:count1 . 'n')<CR><Cmd>lua require('hlslens').start()<CR>]], opts)
-      vim.keymap.set('n', 'N', [[<Cmd>execute('normal! ' . v:count1 . 'N')<CR><Cmd>lua require('hlslens').start()<CR>]], opts)
-      vim.keymap.set('n', '*', [[*<Cmd>lua require('hlslens').start()<CR>]], opts)
-      vim.keymap.set('n', '#', [[#<Cmd>lua require('hlslens').start()<CR>]], opts)
-      vim.keymap.set('n', 'g*', [[g*<Cmd>lua require('hlslens').start()<CR>]], opts)
-      vim.keymap.set('n', 'g#', [[g#<Cmd>lua require('hlslens').start()<CR>]], opts)
     end
+  },
+  {
+    'nvim-pack/nvim-spectre',
+    cmd = 'Spectre',
+    dependencies = 'nvim-lua/plenary.nvim',
   }
 })
 
