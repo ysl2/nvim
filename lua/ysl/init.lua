4fab0a145f7adee18c54ff1fd37524315fdc62f4
ysl2 <www.songli.yu@gmail.com>
2023-08-02 09:17:40 +0800

    Change hop.nvim keybindings; Add hlslens.

--- lua/ysl/init.lua
+++ lua/ysl/init.lua
@@ -177,8 +177,8 @@ vim.list_extend(M, {
     'phaazon/hop.nvim',
     event = 'VeryLazy',
     keys = {
-      { '<A-s>', '<CMD>HopChar1MW<CR>',   mode = '', silent = true },
-      { '<A-w>',    '<CMD>HopPatternMW<CR>', mode = '', silent = true }
+      { '<C-s>', '<CMD>HopChar1MW<CR>',   mode = '', silent = true },
+      { '<A-s>',    '<CMD>HopPatternMW<CR>', mode = '', silent = true }
     },
     config = function()
       require('hop').setup()
@@ -933,6 +933,19 @@ vim.list_extend(M, {
       vim.g.mkdp_theme = 'light'
     end
   },
+  {
+    'kevinhwang91/nvim-hlslens',
+    config = function()
+      require('hlslens').setup()
+      local opts = {noremap = true, silent = true}
+      vim.keymap.set('n', 'n', [[<Cmd>execute('normal! ' . v:count1 . 'n')<CR><Cmd>lua require('hlslens').start()<CR>]], opts)
+      vim.keymap.set('n', 'N', [[<Cmd>execute('normal! ' . v:count1 . 'N')<CR><Cmd>lua require('hlslens').start()<CR>]], opts)
+      vim.keymap.set('n', '*', [[*<Cmd>lua require('hlslens').start()<CR>]], opts)
+      vim.keymap.set('n', '#', [[#<Cmd>lua require('hlslens').start()<CR>]], opts)
+      vim.keymap.set('n', 'g*', [[g*<Cmd>lua require('hlslens').start()<CR>]], opts)
+      vim.keymap.set('n', 'g#', [[g#<Cmd>lua require('hlslens').start()<CR>]], opts)
+    end
+  }
 })
 
 my_load(M)
