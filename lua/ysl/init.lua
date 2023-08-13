ffbd671a04a45c9a5a681597ee7187589e6ce05d
ysl2 <www.songli.yu@gmail.com>
2023-08-13 22:32:48 +0800

    Add <Leader>F to find all files.

--- lua/ysl/init.lua
+++ lua/ysl/init.lua
@@ -471,6 +471,7 @@ vim.list_extend(M, {
     cmd = 'Telescope',
     keys = {
       { '<LEADER>f', '<CMD>Telescope find_files<CR>',                 mode = 'n', silent = true },
+      { '<LEADER>F', '<CMD>Telescope find_files hidden=true<CR>',     mode = 'n', silent = true },
       { '<LEADER>b', '<CMD>Telescope buffers<CR>',                    mode = 'n', silent = true },
       { '<LEADER>s', '<CMD>Telescope live_grep<CR>',                  mode = 'n', silent = true },
       { '<LEADER>G', '<CMD>Telescope git_status<CR>',                 mode = 'n', silent = true },
