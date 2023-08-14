de0378e86a8ae8cd83e6de263c74ecc3574157ae
ysl2 <www.songli.yu@gmail.com>
2023-08-14 10:40:37 +0800

    Change inc-rename to VeryLazy

--- lua/ysl/lsp/nvim_lsp.lua
+++ lua/ysl/lsp/nvim_lsp.lua
@@ -311,7 +311,7 @@ return {
   },
   {
     'smjonas/inc-rename.nvim',
-    event = { 'BufReadPost', 'BufNewFile' },
+    event = 'VeryLazy',
     config = function()
       require('inc_rename').setup()
       vim.api.nvim_create_autocmd('LspAttach', {
