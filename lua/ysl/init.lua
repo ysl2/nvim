1aec018b9087976c1e09f71a914f4ff0b696e753
ysl2 <www.songli.yu@gmail.com>
2023-08-16 10:56:18 +0800

    Fix nvim-surround indent.

--- lua/ysl/init.lua
+++ lua/ysl/init.lua
@@ -213,14 +213,14 @@ local M = {}
 -- ===
 vim.list_extend(M, {
   {
-      'kylechui/nvim-surround',
-      version = '*', -- Use for stability; omit to use `main` branch for the latest features
-      event = 'VeryLazy',
-      config = function()
-          require('nvim-surround').setup({
-              -- Configuration here, or leave empty to use defaults
-          })
-      end
+    'kylechui/nvim-surround',
+    version = '*', -- Use for stability; omit to use `main` branch for the latest features
+    event = 'VeryLazy',
+    config = function()
+      require('nvim-surround').setup({
+        -- Configuration here, or leave empty to use defaults
+      })
+    end
   },
   {
     'numToStr/Comment.nvim',
