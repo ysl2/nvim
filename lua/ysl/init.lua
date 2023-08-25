ff6e097fd367cb21169d233eeed253abafd1ae55
ysl2 <www.songli.yu@gmail.com>
2023-08-25 23:24:25 +0800

    Add bash into treesitter

--- lua/ysl/init.lua
+++ lua/ysl/init.lua
@@ -383,7 +383,7 @@ vim.list_extend(M, {
 
       require('nvim-treesitter.configs').setup {
         -- A list of parser names, or "all"
-        ensure_installed = { 'vim', 'query', 'lua', 'python' },
+        ensure_installed = { 'vim', 'query', 'lua', 'python', 'bash' },
 
         -- Install parsers synchronously (only applied to `ensure_installed`)
         sync_install = false,
