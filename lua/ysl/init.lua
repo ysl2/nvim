7edf774fccc717130dec9b6388511ff49907c63f
ysl2 <www.songli.yu@gmail.com>
2023-08-27 18:53:10 +0800

    Add c into treesitter

--- lua/ysl/init.lua
+++ lua/ysl/init.lua
@@ -380,7 +380,7 @@ vim.list_extend(M, {
 
       require('nvim-treesitter.configs').setup {
         -- A list of parser names, or "all"
-        ensure_installed = { 'vim', 'query', 'lua', 'python', 'bash' },
+        ensure_installed = { 'vim', 'query', 'lua', 'python', 'bash', 'c' },
 
         -- Install parsers synchronously (only applied to `ensure_installed`)
         sync_install = false,
