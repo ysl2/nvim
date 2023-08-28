7961b8dd0934073e3627050332473c2c95a20c8c
ysl2 <www.songli.yu@gmail.com>
2023-08-28 18:23:48 +0800

    Temp save. Bug here!!!

--- scripts/coc_snippets.py
+++ scripts/coc_snippets.py
@@ -2,27 +2,31 @@ import pathlib
 import os
 assert os.sep == '/', 'Not implemented yet for Windows system.'
 import argparse
+import json
+from rich.pretty import pprint as print
 
 
-snippets = pathlib.Path.home() / '.local/share/nvim/lazy/friendly-snippets/snippets'
+friendlyy = pathlib.Path.home() / '.local/share/nvim/lazy/friendly-snippets'
+snippets = friendlyy / 'snippets'
 
 
-def symlink(link, json, create=True):
+def symlink(link, file, create=True):
     try:
         if create:
             link.parent.mkdir(parents=True, exist_ok=True)
-            link.symlink_to(json)
+            link.symlink_to(file)
         else:
             link.unlink()
     except Exception as e:
-        print(e)
-        import pdb
-        pdb.set_trace()  # HACK: Songli.Yu: ""
+        # print(e)
+        # import pdb
+        # pdb.set_trace()  # HACK: Songli.Yu: ""
+        pass
 
 
 def friendly(create=True):
-    for json in snippets.rglob('*.json'):
-        link = json.relative_to(snippets)
+    for file in snippets.rglob('*.json'):
+        link = file.relative_to(snippets)
         ft_index = 0
         if 'frameworks' in link.as_posix():
             ft_index = 1
@@ -30,15 +34,34 @@ def friendly(create=True):
         link = snippets / link
         if link.name.split('.')[0] != ft:
             link = link.parent / link.name.split('.')[0] / f'{ft}.json'
-            symlink(link, json, create)
+            symlink(link, file, create)
 
 
 def cython(create=True):
     cython = pathlib.Path.home() / '.local/share/nvim/lazy/cython-snips'
     python = snippets / 'python'
-    for json in python.glob('*.json'):
-        link = pathlib.Path(f"{cython / json.relative_to(snippets).as_posix().split('.')[0]}/cython.json")
-        symlink(link, json, create)
+    for file in python.glob('*.json'):
+        link = pathlib.Path(f"{cython / file.relative_to(snippets).as_posix().split('.')[0]}/cython.json")
+        symlink(link, file, create)
+
+
+def friendly2(create=True):
+    fp = friendlyy / 'package.json'
+    with open(fp, 'r') as j:
+        fp = json.load(j)
+    fp = fp['contributes']['snippets']
+    for item in fp:
+        file = friendlyy / item['path']
+        language = item['language']
+        if not isinstance(language, (list, tuple)):
+            language = [language]
+        links = []
+        for l in language:
+            filename = file.name.split('.')[0]
+            link = file.parent / filename / f'{l}.json' if filename != l else file
+            links.append(link)
+        for link in links:
+            symlink(link, file, create)
 
 
 if __name__ == '__main__':
