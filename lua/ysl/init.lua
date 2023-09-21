49ea045fd52c3be40adec2c63214c20228c70e06
ysl2 <www.songli.yu@gmail.com>
2023-09-21 16:23:19 +0800

    Add ft='tex' for vimtex to fully disable it.

--- lua/ysl/init.lua
+++ lua/ysl/init.lua
@@ -1601,6 +1601,7 @@ vim.list_extend(M, {
   },
   {
     'lervag/vimtex',
+    ft = 'tex',
     config = function()
       vim.g.vimtex_view_method = 'zathura'
       vim.g.vimtex_format_enabled = 1
