95302a7fde73ff612c4f5630e5b0b849ade1f303
ysl2 <www.songli.yu@gmail.com>
2023-10-20 11:47:36 +0800

    init

--- lua/ysl/init.lua
+++ lua/ysl/init.lua
@@ -1837,6 +1837,16 @@ vim.list_extend(M, {
     config = function()
       require('femaco').setup()
     end
+  },
+  {
+    'edluffy/hologram.nvim',
+    cond = U.exec('echo $TERM') == 'xterm-kitty',
+    ft = { 'markdown', 'latex' },
+    config = function()
+      require('hologram').setup{
+          auto_display = true -- WIP automatic markdown image display, may be prone to breaking
+      }
+    end
   }
 })
 
