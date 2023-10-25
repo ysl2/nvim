aaaefe2c0e9875f1ea028030c62a30f31e3a7e33
ysl2 <www.songli.yu@gmail.com>
2023-10-25 14:35:57 +0800

    Make venn silent.

--- lua/ysl/init.lua
+++ lua/ysl/init.lua
@@ -1885,12 +1885,12 @@ vim.list_extend(M, {
           vim.b.venn_enabled = true
           vim.cmd[[setlocal ve=all]]
           -- draw a line on HJKL keystokes
-          vim.api.nvim_buf_set_keymap(0, 'n', 'J', '<C-v>j:VBox<CR>', {noremap = true})
-          vim.api.nvim_buf_set_keymap(0, 'n', 'K', '<C-v>k:VBox<CR>', {noremap = true})
-          vim.api.nvim_buf_set_keymap(0, 'n', 'L', '<C-v>l:VBox<CR>', {noremap = true})
-          vim.api.nvim_buf_set_keymap(0, 'n', 'H', '<C-v>h:VBox<CR>', {noremap = true})
+          vim.api.nvim_buf_set_keymap(0, 'n', 'J', '<C-v>j:VBox<CR>', { noremap = true, silent = true })
+          vim.api.nvim_buf_set_keymap(0, 'n', 'K', '<C-v>k:VBox<CR>', { noremap = true, silent = true })
+          vim.api.nvim_buf_set_keymap(0, 'n', 'L', '<C-v>l:VBox<CR>', { noremap = true, silent = true })
+          vim.api.nvim_buf_set_keymap(0, 'n', 'H', '<C-v>h:VBox<CR>', { noremap = true, silent = true })
           -- draw a box by pressing "f" with visual selection
-          vim.api.nvim_buf_set_keymap(0, 'v', 'f', ':VBox<CR>', {noremap = true})
+          vim.api.nvim_buf_set_keymap(0, 'v', 'f', ':VBox<CR>', { noremap = true, silent = true })
         else
           vim.cmd[[setlocal ve=]]
           vim.cmd[[mapclear <buffer>]]
