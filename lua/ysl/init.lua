8a5f0ae181641fd8266a32239725a4296d8ff732
ysl2 <www.songli.yu@gmail.com>
2023-10-25 12:39:44 +0800

    Add venn.nvim

--- lua/ysl/init.lua
+++ lua/ysl/init.lua
@@ -1283,6 +1283,11 @@ vim.list_extend(M, {
     config = function()
       require('ibl').setup({
         scope = { enabled = false },
+        exclude = {
+          filetypes = {
+            'text',
+          },
+        },
       })
     end
   },
@@ -1870,6 +1875,29 @@ vim.list_extend(M, {
   {
     'camnw/lf-vim',
     ft = 'lf'
+  },
+  {
+    'jbyuki/venn.nvim',
+    keys = {
+      { '<Leader>V', function()
+        local venn_enabled = vim.inspect(vim.b.venn_enabled)
+        if venn_enabled == 'nil' then
+          vim.b.venn_enabled = true
+          vim.cmd[[setlocal ve=all]]
+          -- draw a line on HJKL keystokes
+          vim.api.nvim_buf_set_keymap(0, 'n', 'J', '<C-v>j:VBox<CR>', {noremap = true})
+          vim.api.nvim_buf_set_keymap(0, 'n', 'K', '<C-v>k:VBox<CR>', {noremap = true})
+          vim.api.nvim_buf_set_keymap(0, 'n', 'L', '<C-v>l:VBox<CR>', {noremap = true})
+          vim.api.nvim_buf_set_keymap(0, 'n', 'H', '<C-v>h:VBox<CR>', {noremap = true})
+          -- draw a box by pressing "f" with visual selection
+          vim.api.nvim_buf_set_keymap(0, 'v', 'f', ':VBox<CR>', {noremap = true})
+        else
+          vim.cmd[[setlocal ve=]]
+          vim.cmd[[mapclear <buffer>]]
+          vim.b.venn_enabled = nil
+        end
+      end, mode = 'n', silent = true }
+    }
   }
 })
 
