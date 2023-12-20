f5a98a31640bc24df333ffc459637288c8738a45
ysl2 <www.songli.yu@gmail.com>
2023-12-21 01:51:08 +0800

    Split nvim-ts-context-commentstring outside.

--- lua/ysl/init.lua
+++ lua/ysl/init.lua
@@ -307,19 +307,19 @@ vim.list_extend(M, {
     end
   },
   {
+    'JoosepAlviste/nvim-ts-context-commentstring',
+    event = 'VeryLazy',
+    config = function ()
+      vim.g.skip_ts_context_commentstring_module = true
+      require('ts_context_commentstring').setup {
+        enable_autocmd = false,
+      }
+    end
+  },
+  {
     'numToStr/Comment.nvim',
     event = 'VeryLazy',
-    dependencies = {
-      {
-        'JoosepAlviste/nvim-ts-context-commentstring',
-        config = function ()
-          vim.g.skip_ts_context_commentstring_module = true
-          require('ts_context_commentstring').setup {
-            enable_autocmd = false,
-          }
-        end
-      }
-    },
+    dependencies = 'JoosepAlviste/nvim-ts-context-commentstring',
     config = function()
       require('Comment').setup({
         pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
