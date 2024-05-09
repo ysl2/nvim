1afedd9567eb6d6a079d558c38164aed2cc368b4
ysl2 <www.songli.yu@gmail.com>
2024-05-09 19:36:30 +0800

    Add bib into tab control.

--- lua/ysl/init.lua
+++ lua/ysl/init.lua
@@ -51,7 +51,7 @@ vim.api.nvim_create_autocmd('BufWritePre', {
   end
 })
 vim.api.nvim_create_autocmd('FileType', {
-  pattern = { 'lua', 'json', 'markdown', 'sshconfig', 'vim', 'yaml' },
+  pattern = { 'lua', 'json', 'markdown', 'sshconfig', 'vim', 'yaml', 'bib' },
   callback = function()
     vim.opt_local.tabstop = 2
     vim.opt_local.shiftwidth = 2
