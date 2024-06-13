2ca24d752869b169f8115c75015e64fde274a4be
ysl2 <www.songli.yu@gmail.com>
2024-06-14 01:45:20 +0800

    Lazy load nvim-lint

--- lua/ysl/lsp/nvim_lsp.lua
+++ lua/ysl/lsp/nvim_lsp.lua
@@ -451,18 +451,19 @@ return {
   },
   {
     'mfussenegger/nvim-lint',
-    event = { 'BufReadPost', 'BufNewFile' },
+    event = { 'BufReadPre', 'BufReadPost', 'BufNewFile', 'InsertLeave' },
     config = function()
-      require('lint').linters_by_ft = {
-        markdown = {'vale',}
+      local lint = require('lint')
+      lint.linters_by_ft = {
+        markdown = {'markdownlint',}
       }
 
-      vim.api.nvim_create_autocmd({ 'BufWritePost' }, {
+      vim.api.nvim_create_autocmd({ 'BufReadPre', 'BufReadPost', 'BufNewFile', 'InsertLeave' }, {
         callback = function()
 
           -- try_lint without arguments runs the linters defined in `linters_by_ft`
           -- for the current filetype
-          require('lint').try_lint()
+          lint.try_lint()
 
           -- You can call `try_lint` with a linter name or a list of names to always
           -- run specific linters, independent of the `linters_by_ft` configuration
