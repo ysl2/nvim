a86969ed8118e953ef5c2e9a919fcfd1310c5c40
ysl2 <www.songli.yu@gmail.com>
2024-06-17 21:59:38 +0800

    Update comment plugin to ts-comments.nvim

--- lua/ysl/init.lua
+++ lua/ysl/init.lua
@@ -327,25 +327,32 @@ vim.list_extend(M, {
       })
     end
   },
+  -- {
+  --   'JoosepAlviste/nvim-ts-context-commentstring',
+  --   event = 'VeryLazy',
+  --   config = function ()
+  --     vim.g.skip_ts_context_commentstring_module = true
+  --     require('ts_context_commentstring').setup({
+  --       enable_autocmd = false,
+  --     })
+  --   end
+  -- },
+  -- {
+  --   'numToStr/Comment.nvim',
+  --   event = 'VeryLazy',
+  --   dependencies = 'JoosepAlviste/nvim-ts-context-commentstring',
+  --   config = function()
+  --     require('Comment').setup({
+  --       pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
+  --     })
+  --   end,
+  -- },
   {
-    'JoosepAlviste/nvim-ts-context-commentstring',
+    'folke/ts-comments.nvim',
     event = 'VeryLazy',
-    config = function ()
-      vim.g.skip_ts_context_commentstring_module = true
-      require('ts_context_commentstring').setup({
-        enable_autocmd = false,
-      })
-    end
-  },
-  {
-    'numToStr/Comment.nvim',
-    event = 'VeryLazy',
-    dependencies = 'JoosepAlviste/nvim-ts-context-commentstring',
     config = function()
-      require('Comment').setup({
-        pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
-      })
-    end,
+      require('ts-comments').setup()
+    end
   },
   {
     'RRethy/vim-illuminate',
