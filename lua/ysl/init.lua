8934d3d2e3fb60d5efc1b4b9b8e5c3c7f16c7670
songliyu <songliyu@tencent.com>
2024-11-22 10:58:57 +0800

    Fix yaml: disable tab indent.

--- lua/ysl/init.lua
+++ lua/ysl/init.lua
@@ -61,7 +61,7 @@ vim.api.nvim_create_autocmd('FileType', {
   end
 })
 vim.api.nvim_create_autocmd('FileType', {
-  pattern = { 'c', 'cpp', 'go', 'yaml'  },
+  pattern = { 'c', 'cpp', 'go' },
   callback = function()
     vim.opt_local.expandtab = false
   end
